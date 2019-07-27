a527f9beec3c960e9dae9cc08949eb75d8237b22
Dan Fox <fox.j.dan@gmail.com>
2019-07-27 08:58:34 -0700

    Optimized new algorithim, completes countNQueensSolutions(15) in about 4 minutes

--- src/solvers.js
+++ src/solvers.js
@@ -91,7 +91,6 @@ window.findNQueensSolution = function(n) {
         if (queenCounter === n) {
           break;
         }
-
         recurseFunc(i + 1);
         if (queenCounter === n) {
           break;
@@ -136,7 +135,7 @@ window.countNQueensSolutions = function(n) {
     }
   }
   var isConflict = function(row, col) {
-    //Purpose: For any given (row, col) position, will tell you if it has a conflict with another queen
+    //Purpose: For any given (row, col) position, will tell you if it has a conflict with another queen already placed on the board
     if (rowOcc[row] === 1) {
       return true;
     } else if (colOcc[col] === 1) {
@@ -150,12 +149,8 @@ window.countNQueensSolutions = function(n) {
   }
   var populateNextPossiblePos = function(row, col) {
     // Purpose: For any given (row, col) position, will return an array of possible positions on the next row
-    // get array of positions on next row that do not have conflicts
-    // no conflicts, push to arr
-    // return arr
     var nextRowPosArr = [];
     var nextRow = row + 1;
-
     for (var i = 0; i < n; i++) {
       if (!isConflict(nextRow, i)) {
         nextRowPosArr.push(i + (n * nextRow));
@@ -166,29 +161,20 @@ window.countNQueensSolutions = function(n) {
   populate(n);
   var recurseFunc = function(possiblePosArr) {
     possiblePosArr.forEach(function(position) {
-      // console.log(`evaluating position ${position}`);
       var row = Math.floor(position / n);
       var col = position % n;
       if (!isConflict(row, col)) {
-        
         queenCounter++;
-        // console.log(`No conflict at ${position}, current # of queens on board ${queenCounter}`);
-
-        if (queenCounter === n) {
+        if (queenCounter == n) {
           solutionCount++;
         } else {
-          
           rowOcc[row] = 1;        // Marks rows, cols and diagonals as occupied
           colOcc[col] = 1;
           MDOcc[col - row] = 1;
           mDOcc[col + row] = 1;
           nextPosRow = [];
-
           var nextPosArr = populateNextPossiblePos(row, col);
-          // console.log(`Recursing down, wheeeeeeee`);
           recurseFunc(nextPosArr);
-          // console.log(`Came back out the recursion hole`);
-
           rowOcc[row] = 0;    // Marks rows, cols and diagonals as unoccupied
           colOcc[col] = 0;
           MDOcc[col - row] = 0;
@@ -199,55 +185,12 @@ window.countNQueensSolutions = function(n) {
     });
   }
   var start = Date.now();
-
   var initialPos = [];
   for (var i = 0; i < n; i++) {
     initialPos.push(i);
   }
   recurseFunc(initialPos);
-
-  // var solutionCount = 0; //fixme
-  // var queenBoard = new Board({'n':n});
-  // var queenCounter = 0;
-  // var possiblePos = Math.pow(n, 2);
-  // var endPos = 0;
-  // var possibleSolutions = [1,1,1,1,2,10,40,92,352,724,2680,14200,73712,365596,2279184,14772512]
-  // console.log(`finding solutions for n = ${n}`);
-  // var recurseFunc = function(board, startPos) {
-  //   for (let i = startPos; i < startPos + n; i++) {
-  //     board.togglePiece(Math.floor(i / n), i % n);
-  //     queenCounter++;
-  //     if (!board.hasAnyQueenConflictsOn(Math.floor(i / n), i % n)) {
-
-  //       if (queenCounter === n) {
-  //         // return queenBoard;
-  //         solutionCount++;
-  //         // console.log(`Solution %: `, solutionCount / possibleSolutions[n-1] * 100, `%`);
-  //       } else {
-  //         recurseFunc(board, (Math.floor(i / n) + 1) * n);
-  //       }
-
-  //       // return recurseFunc(queenBoard, i + 1);
-  //     }
-  //     board.togglePiece(Math.floor(i / n), i % n);
-  //     queenCounter--;
-  //   }
-
-  // }
-
-  // if (n > 1) {
-  //   for (let i = 0; i < n; i++) {
-  //     queenBoard.togglePiece(0, i);
-  //     queenCounter++;
-  //     recurseFunc(queenBoard, (Math.floor(i / n) + 1) * n);
-  //     queenBoard.togglePiece(0, i);
-  //     queenCounter--;
-  //   }
-  // }
-
-
   var end = Date.now() - start;
-
   console.log('Number of solutions for ' + n + ' queens:', n === 1 ? 1 : solutionCount);
   console.log(`Done, took ${end} ms to find ${n <= 1 ? 1 : solutionCount}`);
   return n <= 1 ? 1 : solutionCount;
